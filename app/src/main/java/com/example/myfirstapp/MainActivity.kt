c61cc03acd44539f3c6ce3fdc71721482eaecc02
seregious <seregious@gmail.com>
2022-05-28 23:22:31 +0300

    add buttons

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -3,14 +3,36 @@ package com.example.myfirstapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
 import android.widget.TextView
+import com.example.myfirstapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var bindingClass: ActivityMainBinding
+    val a = 228
+    val b = 322
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bindingClass = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindingClass.root)
+
+        bindingClass.numbers.text = a.toString() + " и " + b.toString()
+
+        bindingClass.plus.setOnClickListener {
+            bindingClass.result.text = (a + b).toString()
+        }
+
+        bindingClass.minus.setOnClickListener {
+            bindingClass.result.text = (b - a).toString()
+        }
+
+        bindingClass.multiply.setOnClickListener {
+            bindingClass.result.text = (a * b).toString()
+        }
+
         Log.d("hello", "there")
 
 
@@ -18,8 +40,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onClick(view: View) {
-        val helloText  = findViewById<TextView>(R.id.helloText)
-        helloText.text = "Clicked"
     }
 
     override fun onStart() {
